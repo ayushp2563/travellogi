45e47a0022fae1b252b2ffc6faaa241908811a3d
Ayush <ayushprajapati2563@gmail.com>
2022-10-30 21:16:30 +0530

    heroku connection

---
+++ api/client/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,39 @@
+import useFetch from "../../hooks/useFetch";
+import "./propertyList.css";
+
+const PropertyList = () => {
+  const { data, loading, error } = useFetch("/hotels/countByType");
+
+  const images = [
+    "https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
+
+  return (
+    <div className="pList">
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className="pListItem" key={i}>
+                <img src={img} alt="" className="pListImg" />
+                <div className="pListTitles">
+                  <h1>{data[i]?.type}</h1>
+                  <h2>
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PropertyList;
